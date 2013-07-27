4cf2cd3aa63f2e43f7cde7b7216aa79a8e7b2ccd
zackabrahamson <zack.abrahamson@gmail.com>
2013-07-27 15:51:36 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,31 +6,36 @@ describe "Static pages" do
   
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Zachary Abrahamson' }
+    let(:page_title) { '' }
     
-    it { should have_selector('h1', text: 'Zachary Abrahamson') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: '| Home') }
-    
   end
   
   describe "Help page" do 
     before { visit help_path }
-
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
     
+    it_should_behave_like "all static pages"    
   end
     
   describe "About page" do
     before { visit about_path }
-
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('About Us')) }
-
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
+    
+    it_should_behave_like "all static pages"
   end
-  
+  # ZA you still need to modify this block.
   describe "Contact page" do
     before { visit contact_path }
     
